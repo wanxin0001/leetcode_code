9132bc37dfeef3bd250fe0b0f881933fdef79933
Wan Xin <xinwan@usc.edu>
2014-08-11 08:57:43 +0800

    revise

--- Permutations.java
+++ Permutations.java
@@ -5,7 +5,7 @@ Given a collection of numbers, return all possible permutations.
 For example,
 [1,2,3] have the following permutations:
 [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
-*/
+***/
 
 public class Solution {
     public ArrayList<ArrayList<Integer>> permute(int[] num) {
