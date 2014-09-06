f384ed172b7dc43f724c4758cc4fefb49a3f6683
Wan Xin <xinwan@usc.edu>
2014-09-06 15:27:51 -0700

    add

---
+++ Symmetric_Tree.java
@@ -0,0 +1,52 @@
+/*
+Symmetric Tree Total Accepted: 23320 Total Submissions: 72482 My Submissions
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+
+confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+*/
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        
+        return helper(root.left, root.right);
+    }
+    
+    private boolean helper(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left != null && right != null && left.val == right.val) {
+            return helper(left.left, right.right) && helper(left.right, right.left);
+        }
+        
+        return false;
+    }
+}
