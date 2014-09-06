07e0f7cb929d8cd62842c6e53aeaf95547ce929f
Wan Xin <xinwan@usc.edu>
2014-09-06 11:13:26 -0700

    add

--- Template/Binary_Tree_Inorder_Traversal.java
+++ Template/Binary_Tree_Inorder_Traversal.java
@@ -51,3 +51,24 @@ public class Solution {
         }
     }
 }
+
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (root == null) {
+            return result;
+        }
+        
+        helper(root, result);
+        return result;
+    }
+    
+    private void helper(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        helper(root.left, result);
+        result.add(root.val);
+        helper(root.right, result);
+    }
+}
