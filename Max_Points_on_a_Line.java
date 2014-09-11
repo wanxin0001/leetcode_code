06c0bd7f2593fd4dd6643dee0c8a3372e3c21464
Wan Xin <xinwan@usc.edu>
2014-09-11 11:32:29 -0700

    add

--- Max_Points_on_a_Line.java
+++ Max_Points_on_a_Line.java
@@ -1,20 +1,20 @@
-import java.util.*;
-public class Solution {
-    private static class Point {
-        int x;
-        int y;
-        Point() { x = 0; y = 0; }
-       Point(int a, int b) { x = a; y = b; }
-    }
-    public static void main(String[] str) {
-        Solution obj = new Solution();
-        Point[] points = new Point[3];
-        points[0] = new Point(2,3);
-        points[1] = new Point(3,3);
-        points[2] = new Point(-5,3);
-        System.out.println(obj.maxPoints(points));
+/*
+Max Points on a Line Total Accepted: 18785 Total Submissions: 171364 My Submissions
+Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
 
-    }
+Have you been asked this question in an interview? 
+*/
+
+/**
+ * Definition for a point.
+ * class Point {
+ *     int x;
+ *     int y;
+ *     Point() { x = 0; y = 0; }
+ *     Point(int a, int b) { x = a; y = b; }
+ * }
+ */
+public class Solution {
     public int maxPoints(Point[] points) {
         if (points == null || points.length == 0) {
             return 0;
@@ -34,12 +34,9 @@ public class Solution {
                 } else if (points[i].x == points[j].x) {
                     map.put((double)Integer.MIN_VALUE, map.get((double)(Integer.MIN_VALUE)) + 1);
                 } else {
-                    double k = (double) (points[i].y - points[j].y) / (double) (points[i].x - points[j].x);
-                    System.out.println("111");
-                    System.out.println(k);
+                    double k = 0.0 + (double) (points[i].y - points[j].y) / (double) (points[i].x - points[j].x); //Must add 0.0
                     if (map.containsKey(k)) {
                         map.put(k, map.get(k) + 1);
-                        System.out.println("222");
                     } else {
                         map.put(k, 2);
                     }
@@ -50,7 +47,6 @@ public class Solution {
                 
             }
              for (int item : map.values()) { // it calls values()
-                    System.out.println(item + " "+dup);
                     if (max < item + dup) {
                         max = item + dup;
                     }
